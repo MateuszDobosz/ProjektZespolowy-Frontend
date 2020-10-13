f099fb7102956959377a5d707c3346db8d77f592
MatDobDev <mateusz.dobosz3@gmail.com>
2020-10-13 21:47:32 +0200

    Add animalcard component

---
+++ src/components/AnimalCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import Img from "../assets/piesek.png";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AnimalCard = ({ name }) => (
+  <Wrapper>
+    <img src={Img} />
+    <p>{name}</p>
+  </Wrapper>
+);
+
+export default AnimalCard;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Img from "../assets/Group 10.svg";
+import AnimalCard from "../components/AnimalCard";
 
 const Wrapper = styled.div`
   height: calc(100vh - 60px);
@@ -13,9 +14,29 @@ const Wrapper = styled.div`
 const MainImg = styled.img`
   width: 1440px;
 `;
+
+const StyledText = styled.p`
+  font-size: 2.2rem;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 1440px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 30px 0 0 0;
+`;
 const HomePage = () => (
   <Wrapper>
     <MainImg src={Img} alt="Dog" />
+    <StyledText>Szukają domu:</StyledText>
+    <CardWrapper>
+      <AnimalCard name="Tofik" />
+      <AnimalCard name="Tofik" />
+      <AnimalCard name="Tofik" />
+      <AnimalCard name="Tofik" />
+    </CardWrapper>
   </Wrapper>
 );
 
