93cda7614b9e4c6c442d495fb60e6bde81ac710b
MatDobDev <mateusz.dobosz3@gmail.com>
2020-10-27 18:35:02 +0100

    fix

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import StyledLink from "../components/Link";
 import LogoIcon from "../assets/Group.svg";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUserGoogle } from "../actions/userActions";
 
 const NavWrapper = styled.div`
   background-color: #0d9e47;
@@ -34,34 +36,52 @@ const BeltWrapper = styled.nav`
 
 const ListItem = styled.li``;
 
-const Menu = () => (
-  <NavWrapper>
-    <BeltWrapper>
-      <img src={LogoIcon} alt="Logo" />
-      <Wrapper>
-        <ListItem>
-          <StyledLink to="/donates" activeClassName="active1">
-            Wesprzyj nas
-          </StyledLink>
-        </ListItem>
-        <ListItem>
-          <StyledLink to="/walk" activeClassName="active1">
-            Umów się na spacer
-          </StyledLink>
-        </ListItem>
-        <ListItem>
-          <StyledLink to="/animals" activeClassName="active1">
-            Nasi podopieczni
-          </StyledLink>
-        </ListItem>
-        <ListItem>
-          <StyledLink to="/login" activeClassName="active1">
-            Dołącz do nas
-          </StyledLink>
-        </ListItem>
-      </Wrapper>
-    </BeltWrapper>
-  </NavWrapper>
-);
+const Menu = () => {
+  const loggedIn = useSelector((state) => state.userReducer.loggedIn);
+  const user = useSelector((state) => state.userReducer.user);
+
+  const dispatch = useDispatch();
+
+  return (
+    <NavWrapper>
+      <BeltWrapper>
+        <img src={LogoIcon} alt="Logo" />
+        <Wrapper>
+          <ListItem>
+            <StyledLink to="/donates" activeClassName="active1">
+              Wesprzyj nas
+            </StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink to="/walk" activeClassName="active1">
+              Umów się na spacer
+            </StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink to="/animals" activeClassName="active1">
+              Nasi podopieczni
+            </StyledLink>
+          </ListItem>
+          <ListItem>
+            {loggedIn ? (
+              <StyledLink
+                to="/"
+                onClick={() => {
+                  dispatch({ type: "LOG_OUT" });
+                }}
+              >
+                Wyloguj {user.name}
+              </StyledLink>
+            ) : (
+              <StyledLink to="/login" activeClassName="active1">
+                Dołącz do nas
+              </StyledLink>
+            )}
+          </ListItem>
+        </Wrapper>
+      </BeltWrapper>
+    </NavWrapper>
+  );
+};
 
 export default Menu;
